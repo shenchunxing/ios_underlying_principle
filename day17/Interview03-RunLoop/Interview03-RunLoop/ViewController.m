1a3a3319bc1904fa30f44ed9e0b17f69c22a7f68
沈春兴 <519045026@github.com>
2022-06-14 07:47:48 +0800

    no message

--- day17/Interview03-RunLoop/Interview03-RunLoop/ViewController.m
+++ day17/Interview03-RunLoop/Interview03-RunLoop/ViewController.m
@@ -14,7 +14,7 @@
 
 @implementation ViewController
 
-NSMutableDictionary *runloops;
+NSMutableDictionary *runloops; //runloop保存在全局的字典里
 
 void observeRunLoopActicities(CFRunLoopObserverRef observer, CFRunLoopActivity activity, void *info)
 {
@@ -31,7 +31,7 @@ void observeRunLoopActicities(CFRunLoopObserverRef observer, CFRunLoopActivity a
         case kCFRunLoopBeforeWaiting:
             NSLog(@"kCFRunLoopBeforeWaiting");
             break;
-        case kCFRunLoopAfterWaiting:
+        case kCFRunLoopAfterWaiting: //唤醒
             NSLog(@"kCFRunLoopAfterWaiting");
             break;
         case kCFRunLoopExit:
@@ -48,20 +48,21 @@ void observeRunLoopActicities(CFRunLoopObserverRef observer, CFRunLoopActivity a
     
 //    NSRunLoop *runloop;
 //    CFRunLoopRef runloop2;
+//    runloops[thread] = runloop; //runloop里面是线程作为key，runloop作为value
     
-//    runloops[thread] = runloop;
+    NSRunLoop *runloop = [NSRunLoop currentRunLoop];
+    CFRunLoopRef runloop2 = CFRunLoopGetCurrent();
     
-//    NSRunLoop *runloop = [NSRunLoop currentRunLoop];
-//    CFRunLoopRef runloop2 = CFRunLoopGetCurrent();
-    
-//    NSArray *array;
-//    CFArrayRef arry2;
-//
-//    NSString *string;
-//    CFStringRef string2;
-    
-//    NSLog(@"%p %p", [NSRunLoop currentRunLoop], [NSRunLoop mainRunLoop]);
-//    NSLog(@"%p %p", CFRunLoopGetCurrent(), CFRunLoopGetMain());
+    NSArray *array;
+    CFArrayRef arry2;
+
+    NSString *string;
+    CFStringRef string2;
+
+    //第一次获取runloop的时候，会自动创建
+    //NSRunLoop是对CFRunLoopGetCurrent的包装，打印的地址NSRunLoop currentRunLoop]和CFRunLoopGetCurrent是不同的
+    NSLog(@"%p %p", [NSRunLoop currentRunLoop], [NSRunLoop mainRunLoop]);
+    NSLog(@"%p %p", CFRunLoopGetCurrent(), CFRunLoopGetMain());
     
     // 有序的
 //    NSMutableArray *array;
@@ -75,7 +76,7 @@ void observeRunLoopActicities(CFRunLoopObserverRef observer, CFRunLoopActivity a
 //
 //    kCFRunLoopDefaultMode;
 //    NSDefaultRunLoopMode;
-//    NSLog(@"%@", [NSRunLoop mainRunLoop]);
+    NSLog(@"%@", [NSRunLoop mainRunLoop]);
     
     
 //    self.view.backgroundColor = [UIColor redColor];
@@ -93,27 +94,43 @@ void observeRunLoopActicities(CFRunLoopObserverRef observer, CFRunLoopActivity a
 //    CFRelease(observer);
     
     // 创建Observer
-    CFRunLoopObserverRef observer = CFRunLoopObserverCreateWithHandler(kCFAllocatorDefault, kCFRunLoopAllActivities, YES, 0, ^(CFRunLoopObserverRef observer, CFRunLoopActivity activity) {
+    CFRunLoopObserverRef observer = CFRunLoopObserverCreateWithHandler(kCFAllocatorDefault,
+                                                                       kCFRunLoopAllActivities,
+                                                                       YES,
+                                                                       0,
+                                                                       ^(CFRunLoopObserverRef observer, CFRunLoopActivity activity) {
         switch (activity) {
-            case kCFRunLoopEntry: {
+            case kCFRunLoopEntry: { //进入
                 CFRunLoopMode mode = CFRunLoopCopyCurrentMode(CFRunLoopGetCurrent());
                 NSLog(@"kCFRunLoopEntry - %@", mode);
                 CFRelease(mode);
                 break;
             }
                 
-            case kCFRunLoopExit: {
+            case kCFRunLoopExit: { //退出
                 CFRunLoopMode mode = CFRunLoopCopyCurrentMode(CFRunLoopGetCurrent());
                 NSLog(@"kCFRunLoopExit - %@", mode);
                 CFRelease(mode);
                 break;
             }
                 
+//            case kCFRunLoopBeforeTimers:
+//                NSLog(@"kCFRunLoopBeforeTimers");
+//                break;
+//            case kCFRunLoopBeforeSources:
+//                NSLog(@"kCFRunLoopBeforeSources");
+//                break;
+//            case kCFRunLoopBeforeWaiting:
+//                NSLog(@"kCFRunLoopBeforeWaiting");
+//                break;
+//            case kCFRunLoopAfterWaiting: //唤醒
+//                NSLog(@"kCFRunLoopAfterWaiting");
+//                break;
             default:
                 break;
         }
     });
-    // 添加Observer到RunLoop中
+    // 添加Observer到RunLoop中,kCFRunLoopCommonModes同时监听默认和滚动
     CFRunLoopAddObserver(CFRunLoopGetMain(), observer, kCFRunLoopCommonModes);
     // 释放
     CFRelease(observer);
@@ -126,9 +143,13 @@ void observeRunLoopActicities(CFRunLoopObserverRef observer, CFRunLoopActivity a
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
-//    [NSTimer scheduledTimerWithTimeInterval:3.0 repeats:NO block:^(NSTimer * _Nonnull timer) {
-//        NSLog(@"定时器-----------");
-//    }];
+    //这里打断点bt，可以看到函数调用栈里面__CFRUNLOOP_IS_CALLING_OUT_TO_A_SOURCE0_PERFORM_FUNCTION__，证明存在source0
+//    NSLog(@"%s",__func__);
+    
+    //证明存在timers
+    [NSTimer scheduledTimerWithTimeInterval:3.0 repeats:NO block:^(NSTimer * _Nonnull timer) {
+        NSLog(@"定时器-----------");
+    }];
     
 }
 
---
+++ day17/Runloop源码/Runloop源码/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Runloop源码
+//
+//  Created by 沈春兴 on 2022/6/13.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
